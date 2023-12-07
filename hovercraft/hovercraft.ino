63833e5834616d5aa837ba34ec8f0a8bc335e5c9
Ninjago77 <66954546+Ninjago77@users.noreply.github.com>
2023-12-06 16:29:33 -0800

    Update hovercraft.ino

--- hovercraft/hovercraft.ino
+++ hovercraft/hovercraft.ino
@@ -1,11 +1,41 @@
-//this is the code for the hovercraft
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+const int delay_ms = 250;
+
+RF24 radio(7, 8);
+
+const byte address[6] = "00001";
+
+struct TransferData {
+  bool fan1_state;
+  bool fan2_state;
+  // the reason its "signed" is because of bug in c++ with +ve int reassigned to -ve int
+  signed int servo_direction_right; // -1 means left, 0 means idle, +1 means right
+  float throttle; // 0.01 to 1.00 as throttle
+};
+
+TransferData data; // Create an empty Var
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // example values set
+  data.fan1_state = true;
+  data.fan2_state = false;
+  data.servo_direction_right = 0;
+  data.throttle = 0.5;
+
+  radio.write(&data, sizeof(TransferData));
+  delay(delay_ms);
+
+  // REST OF THE TRANSMITTER CODE
 
 }
