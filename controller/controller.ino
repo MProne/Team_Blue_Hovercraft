c25d457e67afd005e11b3997ec43a10731732b74
Ninjago77 <66954546+Ninjago77@users.noreply.github.com>
2023-12-06 16:37:28 -0800

    Update controller.ino

--- controller/controller.ino
+++ controller/controller.ino
@@ -2,6 +2,8 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+const int delay_ms = 250;
+
 RF24 radio(7, 8);
 
 const byte address[6] = "00001";
@@ -19,15 +21,21 @@ TransferData data; // Create an empty Var
 void setup() {
   Serial.begin(9600);
   radio.begin();
-  radio.openReadingPipe(0,address);
+  radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
+  radio.stopListening();
 }
 
 void loop() {
-  if (radio.available()) {
-    radio.read(&data, sizeof(TransferData))
-    // REST OF THE RECIEVER CODE
+  // example values set
+  data.fan1_state = true;
+  data.fan2_state = false;
+  data.servo_direction_right = 0;
+  data.throttle = 0.5;
 
-  }
+  radio.write(&data, sizeof(TransferData));
+  delay(delay_ms);
+
+  // REST OF THE TRANSMITTER CODE
+
 }
