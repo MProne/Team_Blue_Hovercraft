4aa1efc5f9faa8d95435090d5a7574367289caaf
MProne <bren_tom_wong@shaw.ca>
2023-12-08 19:30:21 -0500

    nozzle rotate servo function

--- hovercraft/hovercraft.ino
+++ hovercraft/hovercraft.ino
@@ -1,8 +1,17 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Servo.h>
+
+#define ROTATE_SERVO_PIN 9
+//position to set for servo to be idle
+#define NOZZLE_IDLE_POS 90
+
+Servo nozzleRotateServo;
 
 RF24 radio(7, 8);
+
+
 
 const byte address[6] = "00001";
 
@@ -22,12 +31,34 @@ void setup() {
   radio.openReadingPipe(0,address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
+
+  nozzleRotateServo.attach(ROTATE_SERVO_PIN);
+  nozzleRotateServo.write(NOZZLE_IDLE_POS);
 }
 
 void loop() {
   if (radio.available()) {
-    radio.read(&data, sizeof(TransferData))
+    radio.read(&data, sizeof(TransferData));
     // REST OF THE RECIEVER CODE
 
+  }
+}
+
+//generally speed var shouldn't be a value that can be controlled with the controller. This make the driver's life easier
+//speed var should be tuned and manually coded in to the driver's preference
+void rotateNozzle(int direction, int speed){
+  //speed variable to control how fast the servo continously spins (would make the var a float but map returns longs)
+  speed = map(speed, 0, 100, 0, 90);
+  //-1 = CCW,  0 = idle, 1 = CW
+  switch(direction){
+    case -1:
+      nozzleRotateServo.write(NOZZLE_IDLE_POS + speed);
+      break;
+    case 0:
+      nozzleRotateServo.write(NOZZLE_IDLE_POS);
+      break;
+    case 1:
+      nozzleRotateServo.write(NOZZLE_IDLE_POS - speed);
+      break;
   }
 }
